b90761b132967a1f648363d0e2a8e9f0ce47ba98
Daniil-Solo <daniil.solo1723@gmail.com>
2024-08-03 22:01:04 +0300

    add checking extension and size

---
+++ internal/schemas/base.go
@@ -0,0 +1,11 @@
+package schemas
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(message string) *ErrorResponse {
+	return &ErrorResponse{
+		Message: message,
+	}
+}
